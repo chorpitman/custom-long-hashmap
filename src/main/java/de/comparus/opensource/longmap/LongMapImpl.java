bebe8c9ca8f4d87869c678bd9617e801765c4982
Oleg Chorpita (Mac) <oleg.chorpita@lunapps.com>
2018-09-12 23:56:44 +0300

    implemented put(), remove(), isEmpty(), clear(), hashFunction()

--- src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
+++ src/main/java/de/comparus/opensource/longmap/LongMapImpl.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 public class LongMapImpl<V> implements LongMap<V> {
     private static final int DEFAULT_HASH_MAP_SIZE = 16;
     private Node<V>[] hashTable;
-    private int hashTableSize;
+    private int size;
 
     public LongMapImpl() {
         hashTable = new Node[DEFAULT_HASH_MAP_SIZE];
@@ -16,13 +16,13 @@ public class LongMapImpl<V> implements LongMap<V> {
         //create node
         Node<V> newNode = new Node<>(key, value);
         //get index for hashtable
-        int arrIndex = newNode.hashFunction(key, hashTable.length);
+        int arrIndex = hashFunction(key, hashTable.length);
 
         //empty cell in hashtable
         if (hashTable[arrIndex] == null) {
             hashTable[arrIndex] = new Node<>(0, null);
             hashTable[arrIndex].getNodesList().add(newNode);
-            hashTableSize++;
+            size++;
 
             return value;
         }
@@ -41,7 +41,7 @@ public class LongMapImpl<V> implements LongMap<V> {
             //collision. hashcode same but key and value are different
             if (vNode.getKey() != key && !Objects.equals(vNode.getValue(), value) && Objects.equals(newNode, vNode)) {
                 foundNode.add(newNode);
-                hashTableSize++;
+                size++;
 
                 return value;
             }
@@ -51,16 +51,42 @@ public class LongMapImpl<V> implements LongMap<V> {
     }
 
     public V get(long key) {
+        int arrIndex = hashFunction(key, hashTable.length);
+
         return null;
     }
 
     public V remove(long key) {
         //remove using key
+        int arrIndex = hashFunction(key, hashTable.length);
+
+        if (hashTable[arrIndex] == null) {
+            return null;
+        }
+
+        if (hashTable[arrIndex].getNodesList().size() == 1) {
+            V removeCandidate = hashTable[arrIndex].getNodesList().get(0).getValue();
+            hashTable[arrIndex] = null;
+            size--;
+
+            return removeCandidate;
+        }
+
+        List<Node<V>> nodes = hashTable[arrIndex].getNodesList();
+        for (Node<V> node : nodes) {
+            if (key == node.getKey()) {
+                V removeCandidate = node.getValue();
+                nodes.remove(node);
+
+                return removeCandidate;
+            }
+        }
+
         return null;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public boolean containsKey(long key) {
@@ -80,10 +106,21 @@ public class LongMapImpl<V> implements LongMap<V> {
     }
 
     public long size() {
-        return hashTableSize;
+        return size;
     }
 
     public void clear() {
+        if (size != 0) {
+            for (int i = 0; i < hashTable.length; i++) {
+                if (hashTable[i] != null) {
+                    hashTable[i] = null;
+                    size--;
+                }
+            }
+        }
+    }
 
+    public int hashFunction(long key, int hashTableLength) {
+        return (int) ((31 * 17 + key) % hashTableLength);
     }
 }
