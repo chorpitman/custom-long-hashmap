f9560b13ccb7deef883a4cf5813a15b3cfe24ff7
Oleg Chorpita(PC) <chorpita@gmail.com>
2018-09-14 12:15:23 +0300

    removed unused fields

--- src/main/java/de/comparus/opensource/longmap/Node.java
+++ src/main/java/de/comparus/opensource/longmap/Node.java
@@ -7,7 +7,6 @@ public class Node<V> {
     private List<Node<V>> nodesList;
     private long key;
     private V value;
-//    private int hash;
 
     public Node(long key, V value) {
         this.key = key;
@@ -43,10 +42,4 @@ public class Node<V> {
         this.value = value;
     }
 
-//    public int hashFunction(long key, int hashTableLength) {
-//        hash = 31;
-//        return (int) ((hash * 17 + key) % hashTableLength);
-//    }
-
-    //todo think about equals and hashcode
 }
